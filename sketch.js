064a3b22fcf80889953789b18f46f25163995055
ananya0501 <87258219+ananya0501@users.noreply.github.com>
2021-07-11 12:38:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var box
+function setup() {
+  createCanvas(400,400);
+
+box = createSprite(200,200,40,20);
+
+  console.log(box);
+}
+
+function draw() 
+{
+  background("purple");
+
+
+ 
+  drawSprites();
+
+  if(keyIsDown(RIGHT_ARROW)){
+    box.position.x = box.position.x + 5;
+ } 
+
+ if(keyIsDown(LEFT_ARROW)){
+  box.position.x = box.position.x-5;
+} 
+
+if(keyIsDown(UP_ARROW)){
+  box.position.y = box.position.y-5;
+} 
+
+if(keyIsDown(DOWN_ARROW)){
+  box.position.y = box.position.y+5;
+} 
+}
+
+
+
+
